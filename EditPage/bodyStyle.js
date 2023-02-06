7696437e1f6d6007b2a8fbfa9f9f2f8e19e047dd
AndreyLebedev99 <106434349+AndreyLebedev99@users.noreply.github.com>
2023-02-06 14:21:15 +0300

    Delete EditPage directory

--- EditPage/bodyStyle.js
+++
@@ -1,5 +0,0 @@
-export const bodyStyle = () => {
-	document.querySelector('body').classList.remove('body--dashboard')
-	document.querySelector('body').classList.remove('radial-bg')
-	document.querySelector('body').classList.remove('flex-center')
-}
